b21edb3e5da8c434a103ba23fae6e789e84322dd
jianxue <raining0807@163.com>
2024-05-06 22:29:57 +0800

    chore: do not test file converter

--- bass-files/bass-files-converter/src/test/java/com/starxmind/bass/files/converter/test/ConvertTest.java
+++ bass-files/bass-files-converter/src/test/java/com/starxmind/bass/files/converter/test/ConvertTest.java
@@ -14,6 +14,7 @@ import java.io.IOException;
  * @since 1.0
  */
 public class ConvertTest {
+    // converter
 //    @Test
     public void testPng2JpgConverter() throws IOException {
         Png2JpgConverter png2JpgConverter = new Png2JpgConverter();
